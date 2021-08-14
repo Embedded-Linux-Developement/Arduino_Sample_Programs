62f8ef8b7989b55eb7428f0b0200596717b0ab3a
Jerry <jerrymbc@gmail.com>
2021-08-14 13:29:53 +0530

    After adding initial files

---
+++ ESP_32/Refference/Refferences.md
@@ -0,0 +1,7 @@
+# Refferences for Developement.
+ - This document containes different refferences considrred while developing the project.
+ 
+ # Example Code Refferencess.
+ - For enabling AP+STA in ESP 32:-  https://techtutorialsx.com/2021/01/04/esp32-soft-ap-and-station-modes/
+
+- Multi Tasking :- https://savjee.be/2020/01/multitasking-esp32-arduino-freertos/
---
+++ ESP_32/Water_Flow_Pulse_counter_With_Interrupt/Water_Flow_Pulse_counter_With_Interrupt.ino
@@ -0,0 +1,107 @@
+/*
+  Application:
+  - Interface water flow sensor with ESP32 board.
+  
+  Board:
+  - ESP32 Dev Module
+    https://my.cytron.io/p-node32-lite-wifi-and-bluetooth-development-kit
+
+  Sensor:
+  - G 1/2 Water Flow Sensor
+    https://my.cytron.io/p-g-1-2-water-flow-sensor
+ */
+
+ // include library to read and write from flash memory
+#include <EEPROM.h>
+
+
+#define LED_BUILTIN 2
+#define SENSOR  4
+
+
+// define the number of bytes you want to access
+#define EEPROM_SIZE 1
+
+
+long currentMillis = 0;
+long previousMillis = 0;
+int interval = 1000;
+boolean ledState = LOW;
+float calibrationFactor = 4.5;
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+
+void IRAM_ATTR pulseCounter()
+{
+  pulseCount++;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // initialize EEPROM with predefined size
+  EEPROM.begin(EEPROM_SIZE);
+  
+  ledState = EEPROM.read(0);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(SENSOR, INPUT_PULLUP);
+
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  previousMillis = 0;
+
+  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+}
+
+void loop()
+{
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval) {
+    
+    pulse1Sec = pulseCount;
+    pulseCount = 0;
+
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+    previousMillis = millis();
+
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+    
+    // Print the flow rate for this second in litres / minute
+    Serial.print("Raw Value: ");
+    Serial.print(int(pulse1Sec));  // Print the integer part of the variable
+    Serial.print("L/min");
+    Serial.print("\t"); 
+
+
+    // Print the flow rate for this second in litres / minute
+    Serial.print("Flow rate: ");
+    Serial.print(float(flowRate));  // Print the integer part of the variable
+    Serial.print("L/min");
+    Serial.print("\t");       // Print tab space
+
+    // Print the cumulative total of litres flowed since starting
+    Serial.print("Output Liquid Quantity: ");
+    Serial.print(totalMilliLitres);
+    Serial.print("mL / ");
+    Serial.print(totalMilliLitres / 1000);
+    Serial.println("L");
+  }
+}
