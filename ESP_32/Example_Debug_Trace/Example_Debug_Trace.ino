d8ec54d19f2e2101661947f575adeed74b829ba0
Jerry <jerrymbc@gmail.com>
2021-09-27 00:26:51 +0530

    After completing all testing

--- ESP_32/Example_Debug_Trace/Example_Debug_Trace.ino
+++ ESP_32/Example_Debug_Trace/Example_Debug_Trace.ino
@@ -23,15 +23,84 @@ void loop()
   unsigned long Loop_Index;
   // put your main code here, to run repeatedly:
 
-
+#if 0
 /* Test Case:- 1, 
-   1. Test with half the Queue and each have Moderate Buffer sizes ~67*50 = 3350 Byte
+   1. Test with half the Queue and each have Moderate Buffer sizes ~68*50 = 3400 Byte
  */
 
 for (Loop_Index = 0; Loop_Index < (Max_BackGround_Buffer_Queue/2); Loop_Index++)
 {
    Debug_Trace("Hello I am here, To Test the debug Trace printing_ %03d ", Loop_Index);
 }
+
+#endif
+
+
+#if 0
+/* Test Case:- 2, 
+   1. Test with Full the Queue and each have Moderate Buffer sizes ~68*100 = 6800 Byte
+   2. Grater than the allocated buffer 6144.
+   3. Expecting to waite for buffer to get free on buffer Index 90= ~ 68*90 = 6120 Bytes
+
+ */
+
+for (Loop_Index = 0; Loop_Index < (Max_BackGround_Buffer_Queue); Loop_Index++)
+{
+   Debug_Trace("Hello I am here, To Test the debug Trace printing_ %03d", Loop_Index);
+}
+
+#endif
+
+
+#if 0
+/* Test Case:- 3, 
+   1. Test with full buffer with Queue pending, uses 20 Queue and each buffer upto 368, So 368*20 = 7360 Byte
+   2. Grater than the allocated buffer 6144.
+   3. Expecting to waite for buffer to get free on buffer Index 16= ~ 368*16 = 5888 Bytes
+
+ */
+
+for (Loop_Index = 0; Loop_Index < (20); Loop_Index++)
+{
+   Debug_Trace("Hello printing_ %03d I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace", Loop_Index);
+}
+
+#endif
+
+
+#if 0
+/* Test Case:- 4, 
+   1. Test with full buffer with Queue pending, uses 16 Queue and each buffer upto 368, So 368*20 = 7360 Byte
+   2. Grater than the allocated buffer 6144.
+   3. Expecting to No waite for buffer because shall give give enough delay for complete the printing. 16= ~ 368*16 = 5888 Bytes
+
+ */
+
+for (Loop_Index = 0; Loop_Index < (17); Loop_Index++)
+{
+   Debug_Trace("Hello printing_ %03d I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace", Loop_Index);
+}
+
+#endif
+
+
+#if 1
+/* Test Case:- 5, ( Over Night Test)
+   1. Test with 200 Queue and each have Moderate Buffer sizes ~68*200 and 368*200 = 13600  + 73600 Byte
+   2. Grater than the allocated buffer 6144.
+   3. Expecting to waite for buffer and Queue to get free on buffer abd Required Queues.
+   4. Will get delay, but shhall not expect any and Error, all buffer shall process in paraller.
+
+ */
+
+for (Loop_Index = 0; Loop_Index < (Max_BackGround_Buffer_Queue * 2); Loop_Index++)
+{
+   Debug_Trace("Hello I am here, To Test the debug Trace printing_ %03d", Loop_Index);
+   Debug_Trace("Hello printing_ %03d I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace printing_Hello I am here, To Test the debug Trace", Loop_Index);
+
+}
+
+#endif
 
 
 
